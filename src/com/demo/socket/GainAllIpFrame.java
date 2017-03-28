cdb22814faa344b81f117ea54d5ba6f3ba9651c5
cg <1305753112@qq.com>
2017-03-28 22:40:41 +0800

    can get some ips in local net.

--- src/com/demo/socket/GainAllIpFrame.java
+++ src/com/demo/socket/GainAllIpFrame.java
@@ -2,13 +2,19 @@ package com.demo.socket;
 
 import java.awt.Container;
 import java.awt.Frame;
-import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.security.KeyStore.Entry;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -38,7 +44,14 @@ public class GainAllIpFrame {
 			try {
 				str += "主机名：" + netHelper.getLocalHostName() + "<br/>ip:";
 				for (String ip : netHelper.getAllLocalHostIp()) {
-					str += "\t" + ip + " | ";
+					str += ip + "<br />";
+				}
+				
+				str += "局域网ip与主机名：<br />";
+				List<String> ips = netHelper.getIPs();
+				Map<String,String> mapIps = netHelper.getHostnames(ips);
+				for (Map.Entry<String,String> entry : mapIps.entrySet()) {
+					str += entry.getKey() + ":" + entry.getValue() + "<br />";
 				}
 			} catch (Exception e1) {
 				e1.printStackTrace();
@@ -81,4 +94,81 @@ class NetHelper{
 		}
 		return ips;
 	}
+	/*
+	 * 提取局域网IP
+	 * http://blog.csdn.net/havedream_one/article/details/47071393
+	 */
+	public List<String> getIPs()
+    {
+        List<String> list = new ArrayList<String>();
+        boolean flag = false;
+        int count=0;
+        Runtime r = Runtime.getRuntime();
+        Process p;
+        try {
+            p = r.exec("arp -a");
+            BufferedReader br = new BufferedReader(new InputStreamReader(p
+                    .getInputStream()));
+            String inline;
+            while ((inline = br.readLine()) != null) {
+                if(inline.indexOf("接口") > -1){
+                    flag = !flag;
+                    if(!flag){
+                        //碰到下一个"接口"退出循环
+                        break;
+                    }
+                }
+                if(flag){
+                    count++;
+                    if(count > 2){
+                        //有效IP
+                        String[] str=inline.split(" {4}");
+                        list.add(str[0]);
+                    }
+                }
+                System.out.println(inline);
+            }
+            br.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(list);
+        return list;
+    }
+	/*
+	 * 根据IP提取主机名
+	 * http://blog.csdn.net/havedream_one/article/details/47071393
+	 */
+	public Map<String,String> getHostnames(List<String> ips){
+
+        Map<String,String> map = new HashMap<String,String>();
+        System.out.println("正在提取hostname...");
+        for(String ip : ips){
+            String command = "ping -a " + ip;
+            Runtime r = Runtime.getRuntime();
+            Process p;
+            try {
+                p = r.exec(command);
+                BufferedReader br = new BufferedReader(new InputStreamReader(p
+                        .getInputStream()));
+                String inline;
+                while ((inline = br.readLine()) != null) {
+                    if(inline.indexOf("[") > -1){
+                        int start = inline.indexOf("Ping ");
+                        int end = inline.indexOf("[");
+                        String hostname = inline.substring(start+"Ping ".length(),end-1);
+                        System.out.println(hostname);
+                        map.put(ip,hostname);
+                    }
+                }
+                br.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        System.out.println("提取结束！");
+        return map;
+    }
 }
