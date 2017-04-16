5bbad9d9b15bbad566da46686773506e64413e81
cg <1305753112@qq.com>
2017-04-16 22:15:21 +0800

    add a collection demo

---
+++ src/com/demo/collection/SimpleCollection.java
@@ -0,0 +1,17 @@
+package com.demo.collection;
+
+import java.util.*;
+
+public class SimpleCollection {
+	public static void main(String[] args){
+//		Collection<Integer> c = new ArrayList<Integer>();
+		Collection<Integer> c = new HashSet<Integer>();
+		for(int i = 0; i < 10; i++){
+			c.add(i);
+		}
+		c.add(1);
+		for(Integer i : c){
+			System.out.print(i + ", ");
+		}
+	}
+}
