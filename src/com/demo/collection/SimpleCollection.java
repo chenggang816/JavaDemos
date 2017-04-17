62817b9ebbabbad4c614b8c774c281b8824faf32
chenggang816 <1305753112@qq.com>
2017-04-17 17:09:47 +0800

    add some test code.

--- src/com/demo/collection/SimpleCollection.java
+++ src/com/demo/collection/SimpleCollection.java
@@ -3,6 +3,7 @@ package com.demo.collection;
 import java.util.*;
 
 public class SimpleCollection {
+	static int m;
 	public static void main(String[] args){
 //		Collection<Integer> c = new ArrayList<Integer>();
 		Collection<Integer> c = new HashSet<Integer>();
@@ -12,6 +13,30 @@ public class SimpleCollection {
 		c.add(1);
 		for(Integer i : c){
 			System.out.print(i + ", ");
+		}
+		System.out.println();
+		System.out.print(Arrays.toString(c.toArray()) + "\n");
+		System.out.print(Arrays.toString(c.toArray(new Integer[0])) + "\n");
+		System.out.println(Arrays.toString(new Integer[0]));
+		
+		
+		Random rand = new Random(47);
+		SortedSet<Integer> intset = new TreeSet<Integer>();
+		for(int i = 0; i < 10000; i++){
+			intset.add(rand.nextInt(30));
+		}
+		System.out.print(intset + "\n");
+		
+		int[] ints;
+		int n = 10;
+		ints = new int[n];
+		for(int i:ints){
+			System.out.print(i + ",");
+		}
+		System.out.println("\n" + m + "\n");
+		Queue<String> q = new LinkedList<String>(Arrays.asList("Learning Java day by day".split(" ")));
+		while(q.isEmpty() == false){
+			System.out.print(q.poll() + " ");
 		}
 	}
 }
